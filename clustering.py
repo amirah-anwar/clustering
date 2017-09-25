80edf07ff967edb8a8a4226d69cf6a2eebb748e8
Amirah Anwar <amirahanwar10@gmail.com>
2017-09-25 00:22:22 -0700

    Added recomputation step and comments

--- clustering.py
+++ clustering.py
@@ -6,10 +6,14 @@
 import random
 import numpy as np
 
+#Classifies a data set using K-means Algo,
+#Reads the data file
+#Number of clusters = 3 = k
+#Keeps on calculating the centroids of the data set until centroids converge
 def main():
     # ==============Process Data==================================
 
-    # #load txt file into a ndarray
+    # #load txt file into an array of numpy arrays
 	f = open("clusters.txt", 'r')
 	result_matrix = []
 	for line in f.readlines():
@@ -17,26 +21,47 @@ def main():
 	    arr = np.array(map(float, values_as_strings))
 	    result_matrix.append(arr)
 
-	print "read file:", result_matrix
+	# print "read file:", result_matrix
 
+	#Pre-defined number of clusters
 	k = 3
 
+	#K-mean algorithm for classification
 	centroids = kmeans(result_matrix, k)
-	# print "Calculated centroids", centroids
 
+	print "Calculated centroids"
+	for centroid in centroids:
+		print centroid
 
+#Selects centroids randomly
+#1.Assigns each data point to it's nearest centroid and forms a cluster
+#2.Re-calculates the centroid for each cluster
+#Repeats steps 1 and 2 until centroids converge
 def kmeans(dataSet, k):
 	centroids = random.sample(dataSet, k)
-	print "centroids:", centroids
+	print "randomly selected centroids:", centroids
 
 	prevCentroids = None
-	# while not (prevCentroids == centroids)
-	# prevCentroids = centroids
-	clusters = assignment(dataSet, centroids)
-	print "clusters", clusters
-	# centroids = computation(dataSet, k)
+	#Runs until centroids converge
+	while not np.array_equal(prevCentroids, centroids):
+		prevCentroids = centroids
+		#Assign each data point to it's nearest centroid and forms a cluster
+		clusters = assignment(dataSet, centroids)
+		#Re-calculates the centroid for each cluster
+		centroids = reComputation(clusters)
+		print "re-computed centroids:", centroids
 	return centroids
 
+#Re-calculates the centroid for each cluster
+#by calculating the mean of each cluster
+def reComputation(clusters):
+	newCentroids = []
+	for cluster in clusters:
+		arr = np.array(np.mean(clusters[cluster], axis=0))
+		newCentroids.append(arr)
+	return newCentroids
+
+#Assigns each data point to it's nearest centroid and forms a cluster
 def assignment(dataSet, centroids):
 	clusters = {}
 	for dataPoint in dataSet:
@@ -48,9 +73,11 @@ def assignment(dataSet, centroids):
 			clusters[selectedCluster].append(dataPoint)
 	return clusters
 
+#Finds the closest centroid to a data point
 def closest(dataPoint, centroids):
     return min(centroids, key = lambda centroid: distance(dataPoint, centroid))
 
+#Calculates Euclidean distance of a centroid and a data point
 def distance(x, y):
 	return np.sqrt(sum((x - y) ** 2))
 
