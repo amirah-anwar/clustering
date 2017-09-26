ec864fb97de4d422fd4182143bff3712e8cafa40
Michelle Becerra <michel8@vt.edu>
2017-09-25 23:01:20 -0700

    expect and max

--- gaussian.py
+++ gaussian.py
@@ -5,6 +5,7 @@
 
 import random
 import numpy as np
+from numpy import sum
 import math
 
 def main():
@@ -17,30 +18,70 @@ def main():
 	    result_matrix.append(arr)
 
 	em(result_matrix)
-def em():
+	
+def em(result_matrix):
 	#Initialize
 	mu = ramdom.sample(result_matrix, 3)
-	D = result_matrix.T - mu.T
-	cov = (1/N)*D*D.T
-	phi = [(1.0/3.0),(1.0/3.0), (1.0/3.0)]
-
-def expectation():
-
-
-def maximization(result_matrix):
-	#mu = mu(result_matrix);
-
+	D = result_matrix - mu
 	N = len(result_matrix)
-	
-	
+	cov = (1/N)*D.T*D
+	phi = [(1.0/3.0),(1.0/3.0), (1.0/3.0)]
+	c_factor = math.MAX
+	while old_factor != c_factor:
+		old_factor = c_factor
+		soft_mem = expectation(phi, result_matrix, cov, mu)
+		phi, mu, cov = maximization(soft_mem)
+		c_factor = convergence(phi, mu, cov, result_matrix)
+
+def convergence(phi, mu, cov, data):
+	c_factor = 0.0
+	for i in range(150):
+		sumation = 0.0
+		for k in range(3):
+			sumation += phi[k]*N(data[i], mu[k], cov[k])
+		c_factor += math.log(sumation)
+	return c_factor
+
+def expectation(phi, data, cov, mu):
+	dic = {}
+	rik = []
 	for k in range(3):
 		for i in range(150):
-			r_ic = phi[k]*N(result_matrix[i], mu[k], cov)
+			rik.append(phi[k]*N(data, mu, cov)/r_denom(phi,data,mu, cov))
+		dic[k] = rik
+	return dic
+
+def r_denom(phi,data, mu, cov):
+	sumation = 0.0
+	for i in range(3):
+		sumation += phi[i]*N(data, mu, cov)
+	return sumation
+
+def maximization(soft_mem, data):
+	#mu = mu(result_matrix);
+	phi = {}
+	mu = {}
+	phi_cluster = 0.0
+	temp_data = []
+	mu_cluster = np.array([])
+	N = len(data)
+	cov = {}
+	for k in range(3):
+		for i in range(150):
+			phi_cluster += soft_mem[k][i]
+			temp_data.append(soft_mem[k][i]*data[i])
+		phi[k] = phi_cluster/N
+		mu[k] = sum(temp_data, 0)/phi_cluster
+		D = data - mu[k]
+		cov[k] = (1/N)*D.T*D
+	return phi, mu, cov
+
 def N(x,mu, cov):
 	1/(2*math.pi)*math.pow(np.linalg.det(cov), -0.5)*math.exp(-0.5*(x - mu).T*math.pow(cov,-1)*(x - mu))
-def mu(data):
-	arr = np.array(np.mean(data, axis=0))
-	return arr
+
+# def mu(data):
+# 	arr = np.array(np.mean(data, axis=0))
+# 	return arr
 
 if __name__ == "__main__":
     main()
