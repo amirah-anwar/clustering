8cbbc07accbc484ba640a0e6e987615f65fd260e
Michelle Becerra <michel8@vt.edu>
2017-09-25 21:48:25 -0700

    almost done

--- gaussian.py
+++ gaussian.py
@@ -15,18 +15,27 @@ def main():
 	    values_as_strings = line.split(',')
 	    arr = np.array(map(float, values_as_strings))
 	    result_matrix.append(arr)
-	    gaussian(esult_matrix)
 
-def gaussian(result_matrix):
-	#mu = mu(result_matrix);
+	em(result_matrix)
+def em():
+	#Initialize
 	mu = ramdom.sample(result_matrix, 3)
 	D = result_matrix.T - mu.T
-	N = len(result_matrix)
 	cov = (1/N)*D*D.T
-	phi = [1,(1/2), (1/3)]
+	phi = [(1.0/3.0),(1.0/3.0), (1.0/3.0)]
+
+def expectation():
+
+
+def maximization(result_matrix):
+	#mu = mu(result_matrix);
+
+	N = len(result_matrix)
+	
+	
 	for k in range(3):
 		for i in range(150):
-			y = phi[k]*N(result_matrix[i], mu[k], cov[k])
+			r_ic = phi[k]*N(result_matrix[i], mu[k], cov)
 def N(x,mu, cov):
 	1/(2*math.pi)*math.pow(np.linalg.det(cov), -0.5)*math.exp(-0.5*(x - mu).T*math.pow(cov,-1)*(x - mu))
 def mu(data):
