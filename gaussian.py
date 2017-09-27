66bcc6128ef9f20092a158828363fff66795cbcf
Amirah Anwar <amirahanwar10@gmail.com>
2017-09-27 16:49:00 -0700

    updated gaussian algo, gmm from library

--- gaussian.py
+++ gaussian.py
@@ -7,6 +7,7 @@ import random
 import numpy as np
 from numpy import sum
 import math
+from numpy.linalg import inv
 
 def main():
 	#load txt file into an array of numpy arrays
@@ -17,8 +18,8 @@ def main():
 	    arr = np.array(map(float, values_as_strings))
 	    result_matrix.append(arr)
 	result_matrix = np.array(result_matrix)
-	
-	print gmm(result_matrix)
+	calculated_mu = gmm(result_matrix)
+	print "re_computed mu", calculated_mu
 
 def gmm(result_matrix):
 	#Initialize assuming gaussian parameters, get back soft memb
@@ -26,22 +27,43 @@ def gmm(result_matrix):
 	#Assume/given soft membership, recalculate parameters
 	mu = {}
 	temp = random.sample(result_matrix,3)  #get 1 random points represented in numpy arrs
+	print "random centroids", temp
 	for k in range(3):
 		mu[k] = temp[k]
-	D = {0:result_matrix - mu[0], 1:result_matrix - mu[0], 2:result_matrix - mu[0]} #150X2 - vector of 2 elements
+	print "random mu", mu
+	D = {0:result_matrix - mu[0], 1:result_matrix - mu[1], 2:result_matrix - mu[2]} #150X2 - vector of 2 elements
+	# print "D", D
 	n = float(len(result_matrix))
 	cov = {0: (1.0/n)*np.dot(D[0].T, D[0]), 1: (1.0/n)*np.dot(D[1].T, D[1]), 2: (1.0/n)*np.dot(D[2].T, D[2])}
 	phi = {0:(1.0/3.0),1:(1.0/3.0), 2:(1.0/3.0)}
+	# print "n", n
+	# print "cov", cov
+	# print "phi", phi
+	#For convergence
+	old_factor = None
+	new_factor = convergence(phi, mu, cov, result_matrix)
+	# threshold = 200
+	n = 0
+	list_factor = []
+	list_factor.append(new_factor)
 
-	# #For convergence
-	# old_factor = math.MAX
-	# new_factor = 0.0
-	threshold = 200
-	while threshold > 0:
+	while old_factor < new_factor:
+		old_factor = new_factor
 		soft_mem = expectation(phi, result_matrix, cov, mu)
 		phi, mu, cov = maximization(soft_mem, result_matrix)
-		threshold = threshold - 1
-	return cov
+		# summation = 0.0
+		# for k in range(3):
+		# 	summation += soft_mem[k][0]
+		# print "sum of first rik", summation
+		# print "phi, mu, cov", phi, mu, cov
+		new_factor = convergence(phi, mu, cov, result_matrix)
+		list_factor.append(new_factor)
+		n += 1
+		# threshold = threshold - 1
+
+	print "iterations", n
+	print "convergence factor", list_factor
+	return mu
 
 def convergence(phi, mu, cov, data):
 	c_factor = 0.0
@@ -56,10 +78,10 @@ def expectation(phi, data, cov, mu):
 	dic = {}
 
 	for k in range(3):
-		rik = []
+		temp_rik = []
 		for i in range(150):
-			rik.append(phi[k]*(N(data[i], mu[k], cov[k])/r_denom(phi,data[i],cov, mu)))
-		rik = np.array(rik)
+			temp_rik.append(phi[k]*(N(data[i], mu[k], cov[k])/r_denom(phi,data[i],cov, mu)))
+		rik = np.array(temp_rik)
 		dic[k] = rik
 	return dic
 
@@ -70,7 +92,6 @@ def r_denom(phi,data_i, cov, mu):
 	return sumation
 
 def maximization(soft_mem, data):
-
 	phi = {}
 	mu = {}
 	cov = {}
@@ -78,13 +99,15 @@ def maximization(soft_mem, data):
 
 	for k in range(3):
 		phi_cluster = 0.0
-		mu_cluster = []
+		tmp_mu_cluster = []
+
 		for i in range(150):
 			#For each cluster 
+			# print "soft_mem[k][i]", soft_mem[k][i]
 			phi_cluster += soft_mem[k][i]
-			mu_cluster.append(soft_mem[k][i]*data[i])
+			tmp_mu_cluster.append(soft_mem[k][i]*data[i])
 		phi[k] = phi_cluster/n
-		mu_cluster = np.array(mu_cluster)
+		mu_cluster = np.array(tmp_mu_cluster)
 		mu[k] = sum(mu_cluster, 0)/phi_cluster
 		D = data - mu[k]
 		cov[k] = (1.0/n)*np.dot(D.T,D)
@@ -92,7 +115,7 @@ def maximization(soft_mem, data):
 
 #Normalization of probability function
 def N(x,mu, cov):
-	prob = (1/(2*math.pi))*np.power(np.linalg.det(cov), -0.5)*np.exp(np.dot(np.dot(-0.5*((x - mu).T),np.power(cov,-1)),(x - mu)))
+	prob = (1/(2*math.pi))*np.power(np.linalg.det(cov), -0.5)*np.exp(np.dot(np.dot(-0.5*((x - mu).T),inv(cov)),(x - mu)))
 	return float(prob)
 
 
